249e0a426ea6acc52ca6b0ec9e873893e995213c
AkshayShastrakar <akshay.shastrakar1@gmail.com>
2023-04-26 21:12:59 +0530

    method updated

--- src/routes/register.routes.js
+++ src/routes/register.routes.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const { validateUserAndSendOtp, verifyOtpAndRegister } = require("../controller/register.controller");
 
 router.get("/otp/generate/:mobile", validateUserAndSendOtp);
-router.get("/user/:otp", verifyOtpAndRegister);
+router.post("/user/:otp", verifyOtpAndRegister);
 
 module.exports = router;
