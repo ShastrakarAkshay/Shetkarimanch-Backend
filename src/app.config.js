00b897f98c88d6e571c44de68d6c1e85656b0244
kajal-10N <kajal.nimsatkar2411@gmail.com>
2023-06-29 02:23:21 +0530

    config file updated

--- src/app.config.js
+++ src/app.config.js
@@ -1,4 +1,4 @@
-const URL = `mongodb+srv://Admin:12345@cluster0.px2zl8y.mongodb.net/ShetkiManch?retryWrites=true&w=majority`;
+const URL = `mongodb+srv://Admin:admin@shetkimanch.kybs2ys.mongodb.net/ShetkiManch?retryWrites=true&w=majority`;
 
 const CONFIG = {
   PORT: process.env.PORT || 8000,
@@ -8,10 +8,8 @@ const CONFIG = {
     process.env.REGISTER_OTP_SECRET_KEY || "sm-reg-otp-secret-key",
   AUTH_SECRET_KEY: process.env.AUTH_SECRET_KEY || "sm-auth-token-secret-key",
   OTP_SECRET_KEY: process.env.OTP_SECRET_KEY || "sm-otp-token-secret-key",
-  SERVER_URL:
-    process.env.SERVER_URL ||
-    "http://shetkimanch-env.eba-5kwfspmw.us-east-2.elasticbeanstalk.com",
-  UI_ORIGIN: process.env.UI_ORIGIN || "http://shetkimanch.com",
+  SERVER_URL: process.env.SERVER_URL,
+  UI_ORIGIN: process.env.UI_ORIGIN || "http://localhost:4200",
 };
 
 module.exports = { CONFIG };
