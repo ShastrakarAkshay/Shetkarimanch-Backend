755f72c80681a64ef1ac613cf6ec745e65849a86
kajal-10N <kajal.nimsatkar2411@gmail.com>
2023-07-28 13:24:28 +0530

    chagnes added

--- src/controller/grievience.controller.js
+++ src/controller/grievience.controller.js
@@ -42,6 +42,21 @@ const fetchAllGrievienceByDeptId = (req, res) => {
     .catch((err) => res.status(400).send(err));
 };
 
+const fetchAllGrievienceByDeptAndDesig = (req, res) => {
+  Grievience.find({
+    departmentId: req.query.departmentId,
+    designationId: req.query.designationId,
+  })
+    .then((data) => {
+      if (data) {
+        res.status(200).send(data);
+      } else {
+        res.status(400).send("Invalid data");
+      }
+    })
+    .catch((err) => res.status(400).send(err));
+};
+
 const fetchAllGrieviences = (req, res) => {
   Grievience.find()
     .limit(req.query?.limit || 0)
@@ -94,4 +109,5 @@ module.exports = {
   updateGrievienceById,
   deleteGrievienceById,
   fetchAllGrievienceByDeptId,
+  fetchAllGrievienceByDeptAndDesig,
 };
